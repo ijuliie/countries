e12ce57676a40703c86a6fc8ebbf2ad8d4b89619
Julie Trinh <ijuliie06@gmail.com>
2021-12-01 18:38:44 -0800

    created header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import React, { useState } from 'react';
-// import './App.css';
-import Home from './components/Home/Home';
+import React, { useState } from "react";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import "./App.css";
 
 function App() {
-
   return (
     <div className="App">
-        <Home />
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav className='nav-container'>
+            <h1 className='heading'>Where in the world?</h1>
+        </nav>
+    )
+}
+
+export default Header
