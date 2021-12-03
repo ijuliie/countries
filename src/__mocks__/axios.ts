cab5426126a7eae70aed83a21bf642022bdf020a
Julie Trinh <ijuliie06@gmail.com>
2021-12-03 11:13:34 -0800

    update data for mock testing

--- src/__mocks__/axios.ts
+++ src/__mocks__/axios.ts
@@ -1,14 +1,31 @@
 const mockResponse = {
   data: [
     {
-      name: "Afghanistan",
-      population: 1542514,
-      region: "Asia",
-      capital: "Kabul"
-    },
-  ],
+      name: "Brazil",
+      flag: "https://flagcdn.com/br.svg",
+      nativeName: "Brasil",
+      population: 212559409,
+      region: "Americas",
+      subRegion: "South America",
+      capital: "Brasília",
+      topLevelDomain: ".br",
+      currencies: [
+        {
+          code: "BRL",
+          name: "Brazilian real",
+          symbol: "RS"
+        }
+      ],
+      languages: [
+        {
+          name: "Portuguese"
+        }
+      ],
+      borders: ["ARG", "BOL", "COL"]
+    }
+  ]
 }
 
 export default {
-    get: jest.fn().mockResolvedValue(mockResponse)
+  get: jest.fn().mockResolvedValue(mockResponse)
 }
