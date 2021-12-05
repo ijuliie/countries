69c231b0c2e8975faacccec52a5824987aa5af94
Julie Trinh <ijuliie06@gmail.com>
2021-12-05 12:49:18 -0800

    style details component and implement navigate function to go back

--- src/components/CountryDetails/CountryDetails.tsx
+++ src/components/CountryDetails/CountryDetails.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { useMatch } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from "axios";
 import "./CountryDetails.css";
-var countries = require("i18n-iso-countries");
+import countries from "i18n-iso-countries";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 interface IDetails {
@@ -34,14 +37,20 @@ interface Name {
 const CountryDetails: React.FC = () => {
   const [details, setDetails] = useState<IDetails["data"]>([]);
   const match = useMatch(":name");
+  const navigate = useNavigate()
+  
 
   useEffect(() => {
+    fetchCountryDetails(match?.params.name);
+  }, [match?.params.name]);
+
+  const fetchCountryDetails = (country: any) => {
     axios
-      .get(`https://restcountries.com/v3.1/name/${match?.params.name}`)
+      .get(`https://restcountries.com/v3.1/name/${country}`)
       .then((response) => {
         setDetails(response.data);
       });
-  }, []);
+  };
 
   // loop through object with different keys
   const getNativeName = (data: any) => {
@@ -57,7 +66,18 @@ const CountryDetails: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="details-container">
+      <div className="back-button-wrapper">
+        <button onClick={() => {
+          navigate(-1)
+        }} className="back button">
+          <FontAwesomeIcon
+            style={{ paddingRight: "10px" }}
+            icon={faArrowLeft}
+          />
+          Back
+        </button>
+      </div>
       {details.map((data) => {
         return (
           <div
@@ -65,42 +85,84 @@ const CountryDetails: React.FC = () => {
             data-testid="details"
             className="details-wrapper"
           >
-            <div className="details-img-wrapper">
-              <img className="img" src={data.flags.png} />
+            <div className="box">
+              <div className="details-img-wrapper">
+                <img className="img-details" src={data.flags.png} />
+              </div>
             </div>
-            <div>
+            <div className="box">
               <div>
-                <div>{data.name.common}</div>
-                <div>Native Name: {getNativeName(data.name)}</div>
-                <div>Population: {data.population}</div>
-                <div>Region: {data.region}</div>
-                <div>Sub Region: {data.subregion}</div>
-                <div>Capital: {data.capital}</div>
-                <div>Top Level Domain: {data.tld[0]}</div>
-                <div>Currencies: {getCurrency(data.currencies)}</div>
+                <h2 className="detail-header">{data.name.common}</h2>
+              </div>
+              <div className="details-content">
                 <div>
-                  Languages:{" "}
-                  {Object.entries(data.languages).map(
-                    // 3rd argument in map function refers back to the original array that is being looped through
-                    ([_, val], index, arr) => {
-                      {
-                        // if index is not equal length of array minus 1, add a comma
-                      }
-                      if (index != arr.length - 1) {
-                        return `${val}, `;
-                      } else {
-                        return val;
-                      }
-                    }
-                  )}
+                  <p>
+                    <span className="detail-label">Native Name:</span>{" "}
+                    {getNativeName(data.name)}
+                  </p>
+                  <p>
+                    <span className="detail-label">Population:</span> {data.population}
+                  </p>
+                  <p>
+                    <span className="detail-label">Region:</span> {data.region}
+                  </p>
+                  <p>
+                    <span className="detail-label">Sub Region:</span> {data.subregion}
+                  </p>
+                  <p>
+                    <span className="detail-label">Capital:</span> {data.capital}
+                  </p>
                 </div>
-                <div>
-                  Border Countries:{" "}
+                <div className="details-content-2">
+                  <p>
+                    <span className="detail-label">Top Level Domain:</span>{" "}
+                    {data.tld[0]}
+                  </p>
+                  <p>
+                    <span className="detail-label">Currencies:</span>{" "}
+                    {getCurrency(data.currencies)}
+                  </p>
+                  <p>
+                    <span className="detail-label">Languages:</span>{" "}
+                    {Object.entries(data.languages).map(
+                      // 3rd argument in map function refers back to the original array that is being looped through
+                      ([_, val], index, arr) => {
+                        {
+                          // if index is not equal length of array minus 1, add a comma
+                        }
+                        if (index != arr.length - 1) {
+                          return `${val}, `;
+                        } else {
+                          return val;
+                        }
+                      }
+                    )}
+                  </p>
+                </div>
+              </div>
+              <div style={{ marginTop: "30px" }}>
+                <span className="label">Border Countries: </span>
+                <div style={{ display: "flex", flexWrap: "wrap" }}>
                   {data.borders
                     ? data.borders.map((country) => (
-                        <span key={country}>
-                          {countries.getName(country, "en")}
-                        </span>
+                        <div>
+                          <button
+                            className="border button"
+                            onClick={() =>
+                              fetchCountryDetails(
+                                countries.getName(country, "en")
+                              )
+                            }
+                            key={country}
+                          >
+                            <Link
+                              className="links"
+                              to={`/${countries.getName(country, "en")}`}
+                            >
+                              {countries.getName(country, "en")}
+                            </Link>
+                          </button>
+                        </div>
                       ))
                     : ""}
                 </div>
