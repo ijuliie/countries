432ebedca71cc2ff7ea0cad65e45bcc70947fafb
Julie Trinh <ijuliie06@gmail.com>
2021-12-05 12:49:49 -0800

    style input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import { ICountry as Props } from "../CountriesList/CountriesList";
+import "./Input.css";
 
 interface IProps {
   country: Props["name"];
-  handleChange: React.ChangeEventHandler<HTMLInputElement>
-  handleSubmit: React.FormEventHandler<HTMLFormElement>
+  handleChange: React.ChangeEventHandler<HTMLInputElement>;
+  handleSubmit: React.FormEventHandler<HTMLFormElement>;
 }
 
 const Input: React.FC<IProps> = ({ country, handleSubmit, handleChange }) => {
   return (
-    <form data-testid="search" onSubmit={handleSubmit}>
-      <input
-        data-testid="input"
-        name="country"
-        type="text"
-        value={country.common}
-        onChange={handleChange}
-        placeholder="Search for a country..."
-      />
+    <form data-testid="search"  onSubmit={handleSubmit}>
+      <div className="input-wrapper">
+        <input
+          data-testid="input"
+          className="input"
+          name="country"
+          type="text"
+          value={country.common}
+          onChange={handleChange}
+          placeholder="Search for a country..."
+        />
+      </div>
     </form>
   );
 };
