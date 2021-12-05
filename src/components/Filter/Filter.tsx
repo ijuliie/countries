39d57d46aa329e64cf8e6d9539cc0ce183b8da1e
Julie Trinh <ijuliie06@gmail.com>
2021-12-05 12:45:32 -0800

    style select element

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -9,13 +9,23 @@ const Filter: React.FC<IProps> = ({ handleSelectChange }) => {
   const regions = ["africa", "americas", "asia", "europe", "oceania"];
 
   return (
-    <div>
-      <select data-testid="select" id="region" onChange={handleSelectChange}>
+    <div className="select-wrapper">
+      <select
+        className="select"
+        data-testid="select"
+        id="region"
+        onChange={handleSelectChange}
+      >
         <option data-testid="hidden" className="hidden" value="">
           Filter by Region
         </option>
         {regions.map((region) => (
-          <option data-testid={region} key={region} value={region}>
+          <option
+            className="region"
+            data-testid={region}
+            key={region}
+            value={region}
+          >
             {region}
           </option>
         ))}
